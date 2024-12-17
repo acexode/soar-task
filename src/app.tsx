597b57c0c3efd8340d61a818a47fdbe3d7ebef07
Sir Abubakar <abudawud92@gmail.com>
2024-12-17 21:08:09 +0100

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,21 @@
+import axiosMockAdapter from 'src/_mock/axios-mock';
+import 'src/global.css';
+
+import { Router } from 'src/routes/sections';
+
+import { useScrollToTop } from 'src/hooks/use-scroll-to-top';
+
+import { ThemeProvider } from 'src/theme/theme-provider';
+
+// ----------------------------------------------------------------------
+
+export default function App() {
+  useScrollToTop();
+
+
+  return (
+    <ThemeProvider>
+      <Router />
+    </ThemeProvider>
+  );
+}
